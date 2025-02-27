80fc8abc80355c73cbccc273abaaf1f0792858e1
Utisse <lorenzopasqui@gmail.com>
2025-02-27 13:23:49 +0100

    indent

--- plugins/cmp.nix
+++ plugins/cmp.nix
@@ -46,42 +46,42 @@
         documentation = {border = "solid";};
       };
 
-     #mapping = {
-     #  "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-     #  "<C-j>" = "cmp.mapping.select_next_item()";
-     #  "<C-k>" = "cmp.mapping.select_prev_item()";
-     #  "<C-e>" = "cmp.mapping.abort()";
-     #  "<C-b>" = "cmp.mapping.scroll_docs(-4)";
-     #  "<C-f>" = "cmp.mapping.scroll_docs(4)";
-     #  "<C-Space>" = "cmp.mapping.complete()";
-     #  "<CR>" = "cmp.mapping.confirm({ select = true })";
-     #  "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
-     #};
-            mapping = {
-              "<C-l>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
-              "<C-j>" = "cmp.mapping.select_next_item()";
-              "<C-k>" = "cmp.mapping.select_prev_item()";
-              "<C-e>" = "cmp.mapping.abort()";
-              "<C-v>" = "cmp.mapping.scroll_docs(-4)";
-              "<C-f>" = "cmp.mapping.scroll_docs(4)";
-              "<C-Space>" = "cmp.mapping.complete()";
-              "<CR>" = "cmp.mapping.confirm({ select = true })";
-              "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
-             "<C-h>" = ''
-               cmp.mapping(function()
-                 if require("luasnip").expand_or_locally_jumpable() then
-                   require("luasnip").expand_or_jump()
-                 end
-               end, { 'i', 's' })
-             '';
-             "<C-b>" = ''
-               cmp.mapping(function()
-                 if require("luasnip").locally_jumpable(-1) then
-                   require("luasnip").jump(-1)
-                 end
-               end, { 'i', 's' })
-             '';
-          };
+      #mapping = {
+      #  "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+      #  "<C-j>" = "cmp.mapping.select_next_item()";
+      #  "<C-k>" = "cmp.mapping.select_prev_item()";
+      #  "<C-e>" = "cmp.mapping.abort()";
+      #  "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+      #  "<C-f>" = "cmp.mapping.scroll_docs(4)";
+      #  "<C-Space>" = "cmp.mapping.complete()";
+      #  "<CR>" = "cmp.mapping.confirm({ select = true })";
+      #  "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+      #};
+      mapping = {
+        "<C-l>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        "<C-j>" = "cmp.mapping.select_next_item()";
+        "<C-k>" = "cmp.mapping.select_prev_item()";
+        "<C-e>" = "cmp.mapping.abort()";
+        "<C-v>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-f>" = "cmp.mapping.scroll_docs(4)";
+        "<C-Space>" = "cmp.mapping.complete()";
+        "<CR>" = "cmp.mapping.confirm({ select = true })";
+        "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+        "<C-h>" = ''
+          cmp.mapping(function()
+            if require("luasnip").expand_or_locally_jumpable() then
+              require("luasnip").expand_or_jump()
+            end
+          end, { 'i', 's' })
+        '';
+        "<C-b>" = ''
+          cmp.mapping(function()
+            if require("luasnip").locally_jumpable(-1) then
+              require("luasnip").jump(-1)
+            end
+          end, { 'i', 's' })
+        '';
+      };
     };
   };
 }
