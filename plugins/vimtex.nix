01d135fa79d1b04de687326e60e211095aebe9a1
Utisse <lorenzopasqui@gmail.com>
2025-03-03 14:37:14 +0100

    declared custom texlive package, removed log window

--- plugins/vimtex.nix
+++ plugins/vimtex.nix
@@ -1,8 +1,28 @@
-{pkgs, ...}: {
+{pkgs, ...}: 
+let
+  tex = (pkgs.texlive.combine {
+    inherit (pkgs.texlive) scheme-small collection-langitalian 
+    latexmk
+    titlesec
+    titling
+    pgfplots
+    wrapfig
+    import
+    cancel
+    xifthen
+    transparent
+    cleveref
+    ifmtarg
+    ;
+  });
+in
+{
   plugins.vimtex = {
     enable = true;
+    texlivePackage = tex;
     settings = {
       view_method = "zathura";
+      quickfix_mode = 0;
     };
   };
 }
