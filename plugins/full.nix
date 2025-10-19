63b86959cec48b705491de6cea105da5361721a9
lorev <lorenzopasqui@gmail.com>
2025-10-19 17:21:15 +0200

    feat: added plugin trouble

--- plugins/full.nix
+++ plugins/full.nix
@@ -5,6 +5,7 @@
     ./conform-nvim.nix
     ./gitsigns.nix
     ./which-key.nix
+    ./trouble.nix
     ./blink-cmp.nix
   ];
 }
---
+++ plugins/trouble.nix
@@ -0,0 +1,12 @@
+{...}: {
+  plugins.trouble = {
+    enable = true;
+    settings = {
+      auto_close = true;
+      auto_jump = true;
+      auto_preview = true;
+      focus = true;
+      follow = true;
+    };
+  };
+}
